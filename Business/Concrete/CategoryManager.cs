8ad0ca813507098f5bd06e9f5fe95f06a96affa8
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-13 11:35:14 +0300

    13.12.2021 2nd part

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal; //Dependency with constructor injection.
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //Business codes will be here.
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
