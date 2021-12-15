5984ce9ed35cd44b16b6d74008188cc46b6688e3
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-15 10:58:05 +0300

    3rd part of class

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,7 @@ namespace Business.Constants
 
         public static string ProductAdded = "Product added";
         public static string ProductNameInValid = "Invalid product";
+        internal static string MaintenanceTime = "System is now in maintenance";
+        internal static string ProductListed="products listed";
     }
 }
