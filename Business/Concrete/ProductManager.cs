651d29eb1e9be003768b40f62f26aa355e401f6e
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-15 08:32:39 +0300

    15.12.21

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -22,6 +23,20 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //Business codes
+
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("The product name should be longer than two characters");
+            }
+
+           _productDal.Add(product);
+
+            return new SuccessResult();
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -30,6 +45,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p=> p.CategoryId==id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=> p.ProductId==productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
@@ -41,5 +61,6 @@ namespace Business.Concrete
         {
             return _productDal.GetProductDetails();
         }
+
     }
 }
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+       
+        //get; is read only but it can be set in a constructor. 
+        public Result(bool success, string message):this(success)
+        {
+
+            Message= message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
